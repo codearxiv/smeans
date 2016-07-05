20c00beb3b38dc2994cd4f1b53968ad0a69f4611
PiotrBeben <pdbcas@gmail.com>
2016-07-05 13:51:48 +0100

    Add files via upload

--- FacetComplex.m
+++ FacetComplex.m
@@ -1,6 +1,15 @@
+%Copyright (C) 2016 Piotr Beben
+
+%This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+%This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+%You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/.
+
+
+
+
 classdef FacetComplex < handle
     %A representation of a simplicial complex (and operations on it) 
-    %in terms of its top dimensional facets. To save memory. 
+    %in terms of its top dimensional facets.  
 
  
     
--- test_script.m
+++ test_script.m
@@ -1,14 +1,18 @@
+%Copyright (C) 2016 Piotr Beben
+
+%This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+%This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+%You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/.
+  
+
+
+%%
+
 
 
 %Higher values for sensitivity lead to faster convergence, 
 %though poorer fittings and less stability. 
-%Values between 0 and 0.1 seem to work best.  
-
-%%
-
-
-%%
-
+%Values between 0 and 0.1 seem to work best.
 
 sensitivity = 0.1;
 numPts = 100;
